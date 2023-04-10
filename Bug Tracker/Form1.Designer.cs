4184c45841202d9dd581b17e7b6e907f34eb2b06
Abanob Raffet <102380881+AbaPro@users.noreply.github.com>
2023-04-10 13:55:34 +0200

    Add files via upload

---
+++ Bug Tracker/Form1.Designer.cs
@@ -0,0 +1,338 @@
+﻿
+namespace Bug_Tracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.titletb = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.desctb = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.statuslb = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.addbtn = new System.Windows.Forms.Button();
+            this.removebtn = new System.Windows.Forms.Button();
+            this.devcb = new System.Windows.Forms.ComboBox();
+            this.dark = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(19, 19);
+            this.label1.Margin = new System.Windows.Forms.Padding(10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Title";
+            // 
+            // titletb
+            // 
+            this.titletb.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.titletb.Location = new System.Drawing.Point(134, 19);
+            this.titletb.Name = "titletb";
+            this.titletb.Size = new System.Drawing.Size(200, 20);
+            this.titletb.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(19, 59);
+            this.label2.Margin = new System.Windows.Forms.Padding(10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Description";
+            // 
+            // desctb
+            // 
+            this.desctb.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.desctb.Location = new System.Drawing.Point(134, 59);
+            this.desctb.Multiline = true;
+            this.desctb.Name = "desctb";
+            this.desctb.Size = new System.Drawing.Size(200, 74);
+            this.desctb.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(19, 147);
+            this.label3.Margin = new System.Windows.Forms.Padding(10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Date";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(134, 147);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Location = new System.Drawing.Point(405, 17);
+            this.label4.Margin = new System.Windows.Forms.Padding(10);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Priority";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // statuslb
+            // 
+            this.statuslb.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.statuslb.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.statuslb.FormattingEnabled = true;
+            this.statuslb.Items.AddRange(new object[] {
+            "Fixed",
+            "Working On It",
+            "Need Fix"});
+            this.statuslb.Location = new System.Drawing.Point(556, 77);
+            this.statuslb.Name = "statuslb";
+            this.statuslb.Size = new System.Drawing.Size(120, 43);
+            this.statuslb.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label5.Location = new System.Drawing.Point(407, 77);
+            this.label5.Margin = new System.Windows.Forms.Padding(10);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Status";
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader6,
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listView1.FullRowSelect = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 221);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(716, 326);
+            this.listView1.TabIndex = 10;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "ID";
+            this.columnHeader6.Width = 75;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Title";
+            this.columnHeader1.Width = 121;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "description";
+            this.columnHeader2.Width = 160;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Date";
+            this.columnHeader3.Width = 113;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Priority";
+            this.columnHeader4.Width = 122;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Status";
+            this.columnHeader5.Width = 80;
+            // 
+            // addbtn
+            // 
+            this.addbtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.addbtn.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.addbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.addbtn.Location = new System.Drawing.Point(602, 148);
+            this.addbtn.Name = "addbtn";
+            this.addbtn.Size = new System.Drawing.Size(75, 23);
+            this.addbtn.TabIndex = 11;
+            this.addbtn.Text = "&ADD";
+            this.addbtn.UseVisualStyleBackColor = false;
+            this.addbtn.Click += new System.EventHandler(this.addbtn_Click);
+            // 
+            // removebtn
+            // 
+            this.removebtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.removebtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.removebtn.Location = new System.Drawing.Point(493, 553);
+            this.removebtn.Name = "removebtn";
+            this.removebtn.Size = new System.Drawing.Size(183, 23);
+            this.removebtn.TabIndex = 13;
+            this.removebtn.Text = "&REMOVE SELECTED BUG";
+            this.removebtn.UseVisualStyleBackColor = true;
+            this.removebtn.Click += new System.EventHandler(this.removebtn_Click);
+            // 
+            // devcb
+            // 
+            this.devcb.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.devcb.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.devcb.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.devcb.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.devcb.FormattingEnabled = true;
+            this.devcb.Items.AddRange(new object[] {
+            "Very Low",
+            "Low",
+            "Medium",
+            "High",
+            "Very High"});
+            this.devcb.Location = new System.Drawing.Point(556, 21);
+            this.devcb.Name = "devcb";
+            this.devcb.Size = new System.Drawing.Size(121, 21);
+            this.devcb.TabIndex = 14;
+            // 
+            // dark
+            // 
+            this.dark.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.dark.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.dark.Location = new System.Drawing.Point(12, 553);
+            this.dark.Name = "dark";
+            this.dark.Size = new System.Drawing.Size(75, 23);
+            this.dark.TabIndex = 16;
+            this.dark.Text = "&Dark";
+            this.dark.UseVisualStyleBackColor = true;
+            this.dark.Click += new System.EventHandler(this.dark_Click_1);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
+            this.linkLabel1.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(255)))));
+            this.linkLabel1.Location = new System.Drawing.Point(106, 558);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(123, 13);
+            this.linkLabel1.TabIndex = 17;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "LinkedIn: Abanob Raffet";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.ClientSize = new System.Drawing.Size(740, 588);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.dark);
+            this.Controls.Add(this.devcb);
+            this.Controls.Add(this.removebtn);
+            this.Controls.Add(this.addbtn);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.statuslb);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.desctb);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.titletb);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Bug Tracker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox titletb;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox desctb;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox statuslb;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button addbtn;
+        private System.Windows.Forms.Button removebtn;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ComboBox devcb;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.Button dark;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Bug Tracker/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bug_Tracker
+{
+    public partial class Form1 : Form
+    {
+        int isDark = 0;
+        Color whitesh = Color.FromArgb(240, 240, 240);
+        Color blackesh = Color.FromArgb(51, 51, 51);
+        public Form1()
+        {
+            InitializeComponent();
+            loadFile();
+            // Set the format to custom
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            // Set the custom format string
+            dateTimePicker1.CustomFormat = "dd/MM/yyyy";
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void addbtn_Click(object sender, EventArgs e)
+        {
+            //get the date to make a unique key
+            DateTime now = DateTime.Now;
+            string key = now.ToString("yyyyddHss");
+            //get text from form
+            string title = titletb.Text;
+            string desc = desctb.Text;
+            string dev;
+            string date = dateTimePicker1.Text;
+            string status;
+            //if null set it to empty string
+            object devCb = devcb.SelectedItem;
+            if (devCb != null) dev = devCb.ToString();
+            else dev = "";
+            object statusLb = statuslb.SelectedItem;
+            if (statusLb != null) status = statusLb.ToString();
+            else status = "";
+
+
+            //if not found show message
+            if (title != "" && desc != "" && status != "" && dev!="")
+            {
+                //add to list
+                string[] s = { title, desc, date, dev, status };
+                ListViewItem itm = new ListViewItem(key);
+                itm.SubItems.Add(title);
+                itm.SubItems.Add(desc);
+                itm.SubItems.Add(date);
+                itm.SubItems.Add(dev);
+                itm.SubItems.Add(status);
+                listView1.Items.Add(itm);
+                //set to empty
+                titletb.Text = "";
+                desctb.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("Enter The Required Info");
+            }
+            save();
+        }
+
+        private void removebtn_Click(object sender, EventArgs e)
+        {
+            int n=listView1.SelectedItems.Count;
+            if(n>0)
+               listView1.SelectedItems[0].Remove();
+
+            save();
+        }
+        public void save()
+        {
+            //Create a new instance of the StreamWriter class and create the list view items string
+            StreamWriter writer = new StreamWriter("listViewItems.txt");
+            string listViewItems = "";
+            foreach (ListViewItem item in listView1.Items)
+            {
+                foreach (ListViewItem.ListViewSubItem subItem in item.SubItems)
+                {
+                    listViewItems += subItem.Text + ",";
+                }
+                listViewItems += "\r\n";
+            }
+
+            //Write the list view items to the file and close the StreamWriter
+            writer.Write(listViewItems);
+            writer.Close();
+        }
+        public void loadFile()
+        {
+            string fileName = "listViewItems.txt";
+            if (!File.Exists(fileName))
+            {
+                using (FileStream fs = File.Create(fileName));
+            }
+            StreamReader sr = new StreamReader(fileName);
+            while (!sr.EndOfStream)
+            {
+                string[] row = sr.ReadLine().Split(',');         
+                ListViewItem item = new ListViewItem(row);
+                listView1.Items.Add(item);
+            }
+            sr.Close();
+        }
+
+        private void dark_Click_1(object sender, EventArgs e)
+        {
+            dark.Text = (isDark == 1 ? "Dark" : "Light");
+            this.BackColor = (isDark == 1 ? whitesh : blackesh);
+            label1.ForeColor = (isDark==0 ? whitesh : blackesh);
+            label2.ForeColor = (isDark == 0 ? whitesh : blackesh);
+            label3.ForeColor = (isDark == 0 ? whitesh : blackesh);
+            label4.ForeColor = (isDark == 0 ? whitesh : blackesh);
+            label5.ForeColor = (isDark == 0 ? whitesh : blackesh);
+            listView1.ForeColor = (isDark == 0 ? whitesh : blackesh);
+            listView1.BackColor = (isDark == 1 ? whitesh : blackesh);
+            addbtn.BackColor = (isDark == 1 ? whitesh : blackesh);
+            addbtn.ForeColor= (isDark == 0 ? whitesh : blackesh);
+            dark.ForeColor= (isDark == 0 ? whitesh : blackesh);
+            removebtn.ForeColor= (isDark == 0 ? whitesh : blackesh);
+            int g = (isDark == 0 ? 222 : 0);
+            linkLabel1.LinkColor= Color.FromArgb(0, g, 255);
+            isDark ^= 1;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://linkedin.com/in/abanob-raffet/");
+        }
+
+        private void updatebtn_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
